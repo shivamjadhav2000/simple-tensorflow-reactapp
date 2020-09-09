44273af765b44311c65db38313ad355b40cf2537
shivam jadhav <jadhavshivam0228@gmail.com>
2020-09-09 21:54:42 +0530

    first tensorflow react app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import TensorflowExamp from './components/TensorflowExamp'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Tensorflow model</h1>
+      <header className="App-header">
+      <TensorflowExamp />
+      </header>
+    </div>
+  );
+}
+
+export default App;
